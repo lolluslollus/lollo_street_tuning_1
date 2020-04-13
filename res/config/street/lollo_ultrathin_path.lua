0677e543e7e8592511456239ea739a128296fbe2
lolluslollus <lolluslollus@outlook.com>
2020-04-13 02:58:34 +0200

    add ultrathin path

---
+++ res/config/street/lollo_ultrathin_path.lua
@@ -0,0 +1,26 @@
+function data()
+    return {
+        numLanes = 2,
+        sidewalkWidth = 0.2,
+        sidewalkHeight = .0,
+        streetWidth = 0.1,
+        yearFrom = game.config.busLaneYearFrom,
+        yearTo = 0,
+        upgrade = false,
+        country = false,
+        speed = 20.0,
+        type = 'lollo_ultrathin_path',
+        name = _("Ultrathin path"),
+        desc = _("Ultrathin path with a speed limit of %2%, give it a bus lane to pedestrianise it."),
+        categories = {},
+
+        materials = {
+            streetPaving = {
+                name = 'street/country_new_medium_paving.mtl',
+                size = {1.0, 1.0}
+            },
+        },
+        assets = {},
+        cost = 1.0
+    }
+end
