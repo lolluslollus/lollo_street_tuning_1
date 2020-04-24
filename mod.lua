7568e12a398735887c836dda8acb53a0edc021f8
lolluslollus <lolluslollus@outlook.com>
2020-04-24 14:37:51 +0200

    Update mod.lua

--- mod.lua
+++ mod.lua
@@ -1,7 +1,7 @@
 function data()
     return {
         info = {
-            minorVersion = 8,
+            minorVersion = 9,
             severityAdd = 'NONE',
             severityRemove = 'WARNING',
             name = _('_NAME'),
