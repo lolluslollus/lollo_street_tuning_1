80f9ca6d1449d70ea693827def09f59d4a38bbb2
lolluslollus <lolluslollus@outlook.com>
2020-04-27 23:04:22 +0200

    renames and screenshots

--- res/scripts/pitchUtil.lua
+++ res/scripts/pitchUtil.lua
@@ -1,59 +1,51 @@
 local pitchUtil = {}
 local _maxAngleAbs = 0.36
-local _paramXFactor = 0.004
+local _pitchAdjustmentFactor = 0.004
 
-local _getParamsXAdjusted = function(paramX)
-    return math.max(math.min(paramX * _paramXFactor, _maxAngleAbs), -_maxAngleAbs)
+local _getPitchAdjusted = function(pitch)
+    return math.max(math.min(pitch * _pitchAdjustmentFactor, _maxAngleAbs), -_maxAngleAbs)
 end
 
-local _getXYZPitched = function(paramX, x, y, z, xMin, xMax)
-    --print('LOLLO x, y, z = ', x, y, z)
-    local paramXAdjusted = _getParamsXAdjusted(paramX)
-    -- local result = {x * math.cos(paramXAdjusted), y, math.sin(paramXAdjusted) * ((x - xMin) / (xMax - xMin) + xMin / (xMax - xMin)) + z}
-    -- local result = {x * math.cos(paramXAdjusted), y, math.sin(paramXAdjusted) * x / (xMax - xMin) + z}
+local _getXYZPitched = function(pitch, x, y, z, xMin, xMax)
+    local pitchAdjusted = _getPitchAdjusted(pitch)
+    -- local result = {x * math.cos(pitchAdjusted), y, math.sin(pitchAdjusted) * ((x - xMin) / (xMax - xMin) + xMin / (xMax - xMin)) + z}
+    -- local result = {x * math.cos(pitchAdjusted), y, math.sin(pitchAdjusted) * x / (xMax - xMin) + z}
     local result = {
-        x * math.cos(paramXAdjusted),
+        x * math.cos(pitchAdjusted),
         y,
-        math.sin(paramXAdjusted) * x + z
+        math.sin(pitchAdjusted) * x + z
     }
     return result
 end
 
-pitchUtil.getXYZPitched = function(paramX, tbl)
-    --print('LOLLO pitch params = ', paramX)
-    --require('luadump')(true)(tbl)
-    local result = _getXYZPitched(paramX, tbl[1], tbl[2], tbl[3], -1, 1)
-    --print('LOLLO pitch result = ')
-    --require('luadump')(true)(result)
+pitchUtil.getXYZPitched = function(pitch, tbl)
+    local result = _getXYZPitched(pitch, tbl[1], tbl[2], tbl[3], -1, 1)
     return result
 end
 
-pitchUtil.getIdTransfPitched = function(paramX)
-    local paramXAdjusted = _getParamsXAdjusted(paramX)
+pitchUtil.getIdTransfPitched = function(pitch)
+    local pitchAdjusted = _getPitchAdjusted(pitch)
     local result = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}
-    result[1] = math.cos(paramXAdjusted)
-    result[3] = math.sin(paramXAdjusted)
-    result[9] = math.sin(paramXAdjusted)
-    result[11] = math.cos(paramXAdjusted)
-
-    --print('LOLLO transf pitch result = ')
-    --require('luadump')(true)(result)
+    result[1] = math.cos(pitchAdjusted)
+    result[3] = math.sin(pitchAdjusted)
+    result[9] = math.sin(pitchAdjusted)
+    result[11] = math.cos(pitchAdjusted)
     return result
 end
 
-local _maxPitch4Slider = 100
-pitchUtil.getPitchValues = function()
+local _maxPitch4Slider = 100 -- I need a high value coz the arrow key bumps it by 10 at every click
+pitchUtil.getPitchParamValues = function()
     local result = {}
     for i = -_maxPitch4Slider, _maxPitch4Slider do
         table.insert(result, #result + 1, tostring(i))
     end
     return result
 end
-pitchUtil.getMiddlePitch = function()
+pitchUtil.getMiddlePitchParamValue = function()
     return _maxPitch4Slider
 end
-pitchUtil.getDefaultPitch = function()
-    return pitchUtil.getMiddlePitch()
+pitchUtil.getDefaultPitchParamValue = function()
+    return pitchUtil.getMiddlePitchParamValue()
 end
 
 return pitchUtil
