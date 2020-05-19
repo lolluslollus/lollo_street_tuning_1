29f47a9f043cb36251f9420da4b17cc8fd5fd780
lolluslollus <lolluslollus@outlook.com>
2020-05-19 13:42:11 +0200

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -17,6 +17,7 @@ function data()
 				This mod adds a "street construction" to build chunks of road with useful parameters. These chunks can be single or multiple, arranged in parallel.
 				[h1]Problem C: ugly merges[/h1]
 				This mod adds a "street construction" to build prettier merges. For example, you can merge up to four one-lane streets into a large one, perhaps for a fancy station square arrangement.
+				Merges can be reconfigured after being placed, if the street layout allows it. If a merge misbehaves after an upgrade, change its direction twice (ie reverse and restore) to update it to the latest version.
 				[h1]Problem D: no footpaths[/h1]
 				This mod contains some thin footpaths. You can use them to perform various tricks, eg connecting stations to roads or having people walk across a park. Give them a bus lane to pedestrianise them.
 				[h1]Some handy tips:[/h1]
