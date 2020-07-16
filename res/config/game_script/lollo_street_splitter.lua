edc706141ecd299af19f529d65b3e88a08f57c96
lolluslollus <lolluslollus@outlook.com>
2020-07-16 17:14:18 +0200

    clever splitter works except for edge objects

--- res/config/game_script/lollo_street_splitter.lua
+++ res/config/game_script/lollo_street_splitter.lua
@@ -76,67 +76,67 @@ local function _replaceEdgeDestroyingBuildings(oldEdge)
     api.cmd.sendCommand(cmd, callback)
 end
 
-local function _replaceEdge(streetEdgeEntity)
+local function _replaceEdge(oldEdge)
     -- LOLLO NOTE this replaces the street without destroying the buildings
-    if type(streetEdgeEntity) ~= 'table' then return end
+    if type(oldEdge) ~= 'table' then return end
 
 	local proposal = api.type.SimpleProposal.new()
-	proposal.streetProposal.edgesToRemove[1] = streetEdgeEntity.id
+	proposal.streetProposal.edgesToRemove[1] = oldEdge.id
 
-	local baseEdge = api.engine.getComponent(streetEdgeEntity.id, api.type.ComponentType.BASE_EDGE)
-	local baseEdgeStreet = api.engine.getComponent(streetEdgeEntity.id, api.type.ComponentType.BASE_EDGE_STREET)
+	local baseEdge = api.engine.getComponent(oldEdge.id, api.type.ComponentType.BASE_EDGE)
+	local baseEdgeStreet = api.engine.getComponent(oldEdge.id, api.type.ComponentType.BASE_EDGE_STREET)
 
-	local eo = api.type.SegmentAndEntity.new()
-	eo.entity = -1
-	eo.type = 0
-	eo.comp = baseEdge
-	eo.streetEdge = baseEdgeStreet
+	local newEdge = api.type.SegmentAndEntity.new()
+	newEdge.entity = -1
+	newEdge.type = 0
+	newEdge.comp = baseEdge
+	newEdge.streetEdge = baseEdgeStreet
 	-- eo.streetEdge.streetType = api.res.streetTypeRep.find(streetEdgeEntity.streetType)
 
-    proposal.streetProposal.edgesToAdd[1] = eo
+    proposal.streetProposal.edgesToAdd[1] = newEdge
     print('LOLLO eo = ')
-    debugPrint(eo)
-    -- local sampleeo = 
-    -- {
-    --   entity = -1,
-    --   comp = {
-    --     node0 = 13010,
-    --     node1 = 18753,
-    --     tangent0 = {
-    --       x = -32.318000793457,
-    --       y = 81.757850646973,
-    --       z = 3.0953373908997,
-    --     },
-    --     tangent1 = {
-    --       x = -34.457527160645,
-    --       y = 80.931526184082,
-    --       z = -1.0708819627762,
-    --     },
-    --     type = 0,
-    --     typeIndex = -1,
-    --     objects = { },
-    --   },
-    --   type = 0,
-    --   params = {
-    --     streetType = 23,
-    --     hasBus = false,
-    --     tramTrackType = 0,
-    --     precedenceNode0 = 2,
-    --     precedenceNode1 = 2,
-    --   },
-    --   playerOwned = nil,
-    --   streetEdge = {
-    --     streetType = 23,
-    --     hasBus = false,
-    --     tramTrackType = 0,
-    --     precedenceNode0 = 2,
-    --     precedenceNode1 = 2,
-    --   },
-    --   trackEdge = {
-    --     trackType = -1,
-    --     catenary = false,
-    --   },
-    -- }
+    debugPrint(newEdge)
+    --[[ local sampleNewEdge =
+    {
+      entity = -1,
+      comp = {
+        node0 = 13010,
+        node1 = 18753,
+        tangent0 = {
+          x = -32.318000793457,
+          y = 81.757850646973,
+          z = 3.0953373908997,
+        },
+        tangent1 = {
+          x = -34.457527160645,
+          y = 80.931526184082,
+          z = -1.0708819627762,
+        },
+        type = 0,
+        typeIndex = -1,
+        objects = { },
+      },
+      type = 0,
+      params = {
+        streetType = 23,
+        hasBus = false,
+        tramTrackType = 0,
+        precedenceNode0 = 2,
+        precedenceNode1 = 2,
+      },
+      playerOwned = nil,
+      streetEdge = {
+        streetType = 23,
+        hasBus = false,
+        tramTrackType = 0,
+        precedenceNode0 = 2,
+        precedenceNode1 = 2,
+      },
+      trackEdge = {
+        trackType = -1,
+        catenary = false,
+      },
+    } ]]
 
     local callback = function(res, success)
         print('LOLLO res = ')
@@ -150,7 +150,7 @@ local function _replaceEdge(streetEdgeEntity)
 	api.cmd.sendCommand(cmd, callback)
 end
 
-local function _splitEdge(wholeEdge, nodeMid)
+local function _splitEdgeBak(wholeEdge, nodeMid)
     -- LOLLO NOTE this thing destroys all buildings along the edges that it replaces. This defies the very purpose of this mod!
     local proposal = api.type.SimpleProposal.new()
 
@@ -179,6 +179,75 @@ local function _splitEdge(wholeEdge, nodeMid)
     edge1.type = 0
     edge1.streetEdge = api.type.BaseEdgeStreet.new()
     edge1.streetEdge.streetType = api.res.streetTypeRep.find(wholeEdge.streetType)
+
+    proposal.streetProposal.edgesToAdd[1] = edge0
+    proposal.streetProposal.edgesToAdd[2] = edge1
+    proposal.streetProposal.edgesToRemove[1] = wholeEdge.id
+
+    -- eo = api.type.SimpleStreetProposal.EdgeObject.new()
+    -- eo.left = true
+    -- eo.model = "street/signal_waypoint.mdl"
+    -- eo.playerEntity = game.interface.getPlayer()
+    -- eo.oneWay = false
+    -- eo.param = 0.5
+    -- eo.edgeEntity = -1
+    -- eo.name = "MY Beautiful Signal"
+
+    -- proposal.streetProposal.edgeObjectsToAdd[1] = eo
+
+    local node1 = api.type.NodeAndEntity.new()
+    node1.entity = -3
+    node1.comp.position = api.type.Vec3f.new(nodeMid.position[1], nodeMid.position[2], nodeMid.position[3]) --api.type.Vec3f.new(40.0, 0.0, 0.0)
+
+    proposal.streetProposal.nodesToAdd[1] = node1
+
+
+    local context = api.type.Context:new()
+    context.checkTerrainAlignment = false
+    context.cleanupStreetGraph = true -- default is false, it seems to do nothing
+    context.gatherBuildings = false -- buildings are destroyed anyway
+    context.gatherFields = true
+    context.player = api.engine.util.getPlayer() -- buildings are destroyed anyway
+
+    local callback = function(res, success)
+        print('LOLLO street splitter callback returned res = ')
+        debugPrint(res)
+        --for _, v in pairs(res.entities) do print(v) end
+        print(success)
+    end
+
+    local cmd = api.cmd.make.buildProposal(proposal, context, true) -- true means, ignore errors. Errors are not ignored tho: wrong proposals will be discarded
+    api.cmd.sendCommand(cmd, callback)
+end
+
+local function _splitEdge(wholeEdge, nodeMid)
+    -- LOLLO NOTE this thing destroys all buildings along the edges that it replaces. This defies the very purpose of this mod!
+    local proposal = api.type.SimpleProposal.new()
+
+    local baseEdgeStreet = api.engine.getComponent(wholeEdge.id, api.type.ComponentType.BASE_EDGE_STREET)
+    local edge0 = api.type.SegmentAndEntity.new()
+    edge0.entity = -1
+    edge0.type = 0
+    edge0.comp.node0 = wholeEdge.node0
+    edge0.comp.node1 = -3
+    edge0.comp.tangent0 = api.type.Vec3f.new(wholeEdge.node0tangent[1], wholeEdge.node0tangent[2], wholeEdge.node0tangent[3])
+    edge0.comp.tangent1 = api.type.Vec3f.new(nodeMid.tangent[1], nodeMid.tangent[2], nodeMid.tangent[3])
+    edge0.comp.type = 0
+    -- edge0.comp.typeIndex = 0
+    -- edge0.comp.objects = {{ -1, 1 }} --
+    edge0.streetEdge = baseEdgeStreet
+
+    local edge1 = api.type.SegmentAndEntity.new()
+    edge1.entity = -2
+    edge1.type = 0
+    edge1.comp.node0 = -3
+    edge1.comp.node1 = wholeEdge.node1
+    edge1.comp.tangent0 = api.type.Vec3f.new(nodeMid.tangent[1], nodeMid.tangent[2], nodeMid.tangent[3])
+    edge1.comp.tangent1 = api.type.Vec3f.new(wholeEdge.node1tangent[1], wholeEdge.node1tangent[2], wholeEdge.node1tangent[3])
+    edge1.comp.type = 0
+    -- edge1.comp.typeIndex = 0
+    --edge1.comp.objects = {{ -1, 1 }}
+    edge1.streetEdge = baseEdgeStreet
 
     proposal.streetProposal.edgesToAdd[1] = edge0
     proposal.streetProposal.edgesToAdd[2] = edge1
