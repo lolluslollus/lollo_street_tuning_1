c5e36952e703396a2d8324ecdc0c6aafc795a4d1
lolluslollus <lolluslollus@outlook.com>
2020-07-16 14:55:13 +0200

    Update lollo_street_splitter.lua

--- res/config/game_script/lollo_street_splitter.lua
+++ res/config/game_script/lollo_street_splitter.lua
@@ -91,9 +91,52 @@ local function _replaceEdge(streetEdgeEntity)
 	eo.type = 0
 	eo.comp = baseEdge
 	eo.streetEdge = baseEdgeStreet
-	eo.streetEdge.streetType = api.res.streetTypeRep.find(streetEdgeEntity.streetType)
+	-- eo.streetEdge.streetType = api.res.streetTypeRep.find(streetEdgeEntity.streetType)
 
-	proposal.streetProposal.edgesToAdd[1] = eo
+    proposal.streetProposal.edgesToAdd[1] = eo
+    print('LOLLO eo = ')
+    debugPrint(eo)
+    -- local sampleeo = 
+    -- {
+    --   entity = -1,
+    --   comp = {
+    --     node0 = 13010,
+    --     node1 = 18753,
+    --     tangent0 = {
+    --       x = -32.318000793457,
+    --       y = 81.757850646973,
+    --       z = 3.0953373908997,
+    --     },
+    --     tangent1 = {
+    --       x = -34.457527160645,
+    --       y = 80.931526184082,
+    --       z = -1.0708819627762,
+    --     },
+    --     type = 0,
+    --     typeIndex = -1,
+    --     objects = { },
+    --   },
+    --   type = 0,
+    --   params = {
+    --     streetType = 23,
+    --     hasBus = false,
+    --     tramTrackType = 0,
+    --     precedenceNode0 = 2,
+    --     precedenceNode1 = 2,
+    --   },
+    --   playerOwned = nil,
+    --   streetEdge = {
+    --     streetType = 23,
+    --     hasBus = false,
+    --     tramTrackType = 0,
+    --     precedenceNode0 = 2,
+    --     precedenceNode1 = 2,
+    --   },
+    --   trackEdge = {
+    --     trackType = -1,
+    --     catenary = false,
+    --   },
+    -- }
 
     local callback = function(res, success)
         print('LOLLO res = ')
