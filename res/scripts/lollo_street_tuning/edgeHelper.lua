3e0ee7bec5e370e65b222db51ca9f0fa0345e7d4
lolluslollus <lolluslollus@hotmail.com>
2020-07-26 23:06:02 +0200

    Update edgeHelper.lua

--- res/scripts/lollo_street_tuning/edgeHelper.lua
+++ res/scripts/lollo_street_tuning/edgeHelper.lua
@@ -175,48 +175,57 @@ helper.getNodeBetween = function(node0, node1, betweenPosition)
     -- if node1NormalisationFactor == 0 then node1NormalisationFactor = math.huge else node1NormalisationFactor = 1.0 / node1NormalisationFactor end
     local x0 = node0[1][1]
     local x1 = node1[1][1]
-    local cos0 = node0[2][1]-- * node0NormalisationFactor -- correct but useless
-    local cos1 = node1[2][1]-- * node1NormalisationFactor -- correct but useless
+    -- local cos0 = node0[2][1]-- * node0NormalisationFactor -- correct but useless
+    -- local cos1 = node1[2][1]-- * node1NormalisationFactor -- correct but useless
     local y0 = node0[1][2]
     local y1 = node1[1][2]
-    local sin0 = node0[2][2]-- * node0NormalisationFactor -- correct but useless
-    local sin1 = node1[2][2]-- * node1NormalisationFactor -- correct but useless
-    local theta0 = math.atan2(sin0, cos0)
-    local theta1 = math.atan2(sin1, cos1)
+    -- local sin0 = node0[2][2]-- * node0NormalisationFactor -- correct but useless
+    -- local sin1 = node1[2][2]-- * node1NormalisationFactor -- correct but useless
+    local ypsilon0 = math.atan2(node0[2][2], node0[2][1])
+    local ypsilon1 = math.atan2(node1[2][2], node1[2][1])
     local z0 = node0[1][3]
     local z1 = node1[1][3]
+    local zeta0 = math.atan2(node0[2][3], node0[2][1])
+    local zeta1 = math.atan2(node1[2][3], node1[2][1])
     -- rotate the edges around the Z axis so that y0 = y1
     local zRotation = -math.atan2(y1 - y0, x1 - x0)
     local x0I = x0
     local x1I = x0 + helper.getVectorLength({x1 - x0, y1 - y0, 0})
-    -- local cos0I = math.cos(theta0 + zRotation)
-    -- local cos1I = math.cos(theta1 + zRotation)
+    -- local cos0I = math.cos(ypsilon0 + zRotation)
+    -- local cos1I = math.cos(ypsilon1 + zRotation)
     local y0I = y0
     -- local y1I = y0
-    -- local sin0I = math.sin(theta0 + zRotation)
-    -- local sin1I = math.sin(theta1 + zRotation)
-    -- local theta0I = math.atan2(sin0I, cos0I)
-    -- local theta1I = math.atan2(sin1I, cos1I)
+    -- local sin0I = math.sin(ypsilon0 + zRotation)
+    -- local sin1I = math.sin(ypsilon1 + zRotation)
+    -- local ypsilon0I = math.atan2(sin0I, cos0I)
+    -- local ypsilon1I = math.atan2(sin1I, cos1I)
+    local z0I = z0
+    local z1I = z1
+    -- LOLLO TODO check if you can use the previous zetas or you'd better use the rotated nodeN[2][1]
+    -- local zeta0I = math.atan2(node0[2][3], node0[2][1])
+    -- local zeta1I = math.atan2(node1[2][3], node1[2][1])
 
-    -- Now I can solve the system:
+    local invertedXMatrix = matrixUtils.invert(
+        {
+            {1, x0I, x0I * x0I, x0I * x0I * x0I},
+            {1, x1I, x1I * x1I, x1I * x1I * x1I},
+            {0, 1, 2 * x0I, 3 * x0I * x0I},
+            {0, 1, 2 * x1I, 3 * x1I * x1I}
+        }
+    )
+
+    -- Now I solve the system for y:
     -- a + b x0' + c x0'^2 + d x0'^3 = y0'
     -- a + b x1' + c x1'^2 + d x1'^3 = y0'
     -- b + 2 c x0' + 3 d x0'^2 = sin0' / cos0'
     -- b + 2 c x1' + 3 d x1'^2 = sin1' / cos1'
     local abcdY = matrixUtils.mul(
-        matrixUtils.invert(
-            {
-                {1, x0I, x0I * x0I, x0I * x0I * x0I},
-                {1, x1I, x1I * x1I, x1I * x1I * x1I},
-                {0, 1, 2 * x0I, 3 * x0I * x0I},
-                {0, 1, 2 * x1I, 3 * x1I * x1I}
-            }
-        ),
+        invertedXMatrix,
         {
             {y0I},
             {y0I},
-            {math.tan(theta0 + zRotation)}, -- {sin0I / cos0I}, -- LOLLO TODO risk of division by zero
-            {math.tan(theta1 + zRotation)}, -- {sin1I / cos1I}
+            {math.tan(ypsilon0 + zRotation)}, -- {sin0I / cos0I}, -- LOLLO TODO risk of division by zero
+            {math.tan(ypsilon1 + zRotation)}, -- {sin1I / cos1I}
         }
     )
 
@@ -228,33 +237,50 @@ helper.getNodeBetween = function(node0, node1, betweenPosition)
     local x2I = x0I + (x1I - x0I) * x20Shift
     local y2I = aY + bY * x2I + cY * x2I * x2I + dY * x2I * x2I * x2I
     -- calculate its y derivative:
-    local tan2I = bY + 2 * cY * x2I + 3 * dY * x2I * x2I
+    local yOnX2I = bY + 2 * cY * x2I + 3 * dY * x2I * x2I
     -- Now I undo the rotation I did at the beginning
     local ro2 = helper.getVectorLength({x2I - x0I, y2I - y0I, 0})
     local alpha2I = math.atan2(y2I - y0I, x2I - x0I)
-    local theta2I = math.atan(tan2I)
+    local ypsilon2I = math.atan2(yOnX2I)
+
+    -- Now I solve the system for z:
+    -- a + b x0' + c x0'^2 + d x0'^3 = z0'
+    -- a + b x1' + c x1'^2 + d x1'^3 = z1'
+    -- b + 2 c x0' + 3 d x0'^2 = sin0' / cos0'
+    -- b + 2 c x1' + 3 d x1'^2 = sin1' / cos1'
+    local abcdZ = matrixUtils.mul(
+        invertedXMatrix,
+        {
+            {z0I},
+            {z1I},
+            {math.tan(zeta0)},
+            {math.tan(zeta1)},
+        }
+    )
+    local aZ = abcdZ[1][1]
+    local bZ = abcdZ[2][1]
+    local cZ = abcdZ[3][1]
+    local dZ = abcdZ[4][1]
+    -- Now I take x2' between x0' and x1',
+    local z2I = aZ + bZ * x2I + cZ * x2I * x2I + dZ * x2I * x2I * x2I
+    -- calculate its y derivative:
+    local zOnX2I = bZ + 2 * cZ * x2I + 3 * dZ * x2I * x2I
+    local zeta2I = math.atan(zOnX2I)
 
     local nodeBetween = {
         position = {
             x0I + ro2 * math.cos(alpha2I - zRotation),
             y0I + ro2 * math.sin(alpha2I - zRotation),
-            0
+            z2I
         },
         tangent = {
-            math.cos(theta2I - zRotation), -- * ro2,
-            math.sin(theta2I - zRotation), -- * ro2,
-            0
+            math.cos(ypsilon2I - zRotation), -- * ro2,
+            math.sin(ypsilon2I - zRotation), -- * ro2,
+            math.sin(zeta2I)
         }
     }
-    -- add Z
-    nodeBetween.position[3] = betweenPosition[3]
-    -- add Z tan -- LOLLO TODO this is a linear interpolation, the game uses a 3rd degree polynom: use one for smoother gradients if needed
-    nodeBetween.tangent[3] = node01Distance ~= 0
-    and
-        (node0[2][3] + (node1[2][3] - node0[2][3]) * x20Shift) / node01Distance
-    or
-        0
 
+    -- normalise tangent
     local tangentLength = helper.getVectorLength(nodeBetween.tangent)
     if tangentLength ~= 0 and tangentLength ~= 1 then
         nodeBetween.tangent[1] = nodeBetween.tangent[1] / tangentLength
