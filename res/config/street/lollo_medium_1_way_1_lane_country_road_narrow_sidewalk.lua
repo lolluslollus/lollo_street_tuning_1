262d060c672ac8541849be412a1d09cfc78ca744
lolluslollus <lolluslollus@outlook.com>
2020-08-11 04:53:52 +0200

    Update lollo_medium_1_way_1_lane_country_road_narrow_sidewalk.lua

--- res/config/street/lollo_medium_1_way_1_lane_country_road_narrow_sidewalk.lua
+++ res/config/street/lollo_medium_1_way_1_lane_country_road_narrow_sidewalk.lua
@@ -22,14 +22,22 @@
             streetPaving = {
                 name = "street/country_new_medium_paving.mtl",
                 size = { 8.0, 8.0 }
-            },		
+            },
+            -- streetBorder = {
+            --     name = "street/country_new_small_street_border.mtl",
+            --     size = { 9.0, 0.56 }
+            -- },
             streetBorder = {
-                name = "street/country_new_small_street_border.mtl",		
-                size = { 9.0, 0.56 }		
-            },			
+                name = 'street/country_new_large_border.mtl',
+                size = {24, 0.459}
+            },
             streetLane = {
                 name = "street/country_new_medium_lane.mtl",
                 size = { 2.5, 2.5 }
+            },
+            streetArrow = {
+                name = 'street/default_arrows.mtl',
+                size = {6.0, 3.0}
             },
             streetStripe = {
     
@@ -49,9 +57,13 @@
                 name = "street/new_medium_tram_track.mtl",
                 size = { 2.0, 2.0 }
             },
+            -- junctionBorder = {
+            --     name = "street/country_new_small_street_border.mtl",
+            --     size = { 9.0, 0.56 }
+            -- },
             junctionBorder = {
-                name = "street/country_new_small_street_border.mtl",		
-                size = { 9.0, 0.56 }		
+                name = 'street/country_new_large_border.mtl',
+                size = {24, 0.459}
             },
             crossingLane = {
                 name = "street/country_new_medium_lane.mtl",
