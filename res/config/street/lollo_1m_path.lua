2fefda2beae26aca9c17e34b329ad327fdc9974d
lolluslollus <lolluslollus@outlook.com>
2020-09-21 19:32:08 +0200

    add 1m path

---
+++ res/config/street/lollo_1m_path.lua
@@ -0,0 +1,42 @@
+function data()
+    return {
+        numLanes = 2,
+        -- transportModesStreet = {'PERSON'}, -- dumps
+        -- transportModesStreet = {'TRUCK'}, -- it dumps with an empty array or with {'PERSON'}; savegames dump with {'TRUCK'}
+        sidewalkWidth = 0.4,
+        sidewalkHeight = 0.0,
+        streetWidth = 0.2,
+        yearFrom = game.config.busLaneYearFrom,
+        yearTo = 0,
+        upgrade = false,
+        country = false,
+        speed = 20.0,
+        type = 'lollo_1m_path',
+        name = _("1 Metre Path"),
+        desc = _("1 metre path with a speed limit of %2%, give it a bus lane to pedestrianise it."),
+        categories = { 'paths' },
+        order = 1,
+        busAndTramRight = true,
+        -- slopeBuildSteps = 1,
+        embankmentSlopeLow  = 0.75,
+        embankmentSlopeHigh  = 2.5,
+        borderGroundTex = "street_border.lua",
+        materials = {
+            -- sidewalkBorderOuter = {
+            --     name = "street/old_medium_sidewalk_border_outer.mtl",
+            --     -- size = { 16.0, 0.41602 }
+            --     size = { 16.0, 0.2 }
+            -- },
+            sidewalkPaving = {
+                name = "street/old_medium_sidewalk.mtl",
+                size = { 4.0, 4.0 }
+            },
+            streetPaving = {
+                name = "street/old_medium_sidewalk.mtl",
+                size = { 4.0, 4.0 }
+            },
+        },
+        assets = {},
+        cost = 1.0
+    }
+end
