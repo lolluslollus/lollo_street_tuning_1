0855ec9efb2e092bccb20b55b8fbce2812583ee4
lolluslollus <lolluslollus@outlook.com>
2020-11-20 20:37:19 +0100

    Update edgeHelper.lua

--- res/scripts/lollo_street_tuning/edgeHelper.lua
+++ res/scripts/lollo_street_tuning/edgeHelper.lua
@@ -57,11 +57,11 @@ local function swap(num1, num2)
     num2 = swapTemp
 end
 
-helper.getNearestEdgeId = function(transf)
+helper.getNearestEdgeId = function(transf, searchRadius)
     if type(transf) ~= 'table' then return nil end
 
     local _position = transfUtils.getVec123Transformed({0, 0, 0}, transf)
-    local _searchRadius = 0.5
+    local _searchRadius = searchRadius or 0.5
     local _box0 = api.type.Box3.new(
         api.type.Vec3f.new(_position[1] - _searchRadius, _position[2] - _searchRadius, -9999),
         api.type.Vec3f.new(_position[1] + _searchRadius, _position[2] + _searchRadius, 9999)
