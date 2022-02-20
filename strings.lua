ac96a39c9041153e0aab5eff5e27c35d101ae273
lolluslollus <lolluslollus@hotmail.com>
2022-02-20 12:32:31 +0100

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -5,13 +5,13 @@ function data()
 				[b]This is a collection of tools to fine-tune the road vehicle routing and the streets.[/b]
 
 				[h1]Problem A: inflexible roads[/h1]
-				This mod adds a selection of streets with multiple lanes, to allow overtaking, but with the same width, so you can try different road types without destroying your buildings. Some road types reserve the right lane for passenger or cargo vehicles, they carry extra signs on the pavement.
-				Use the categories to select the right street type, for ease of navigation.
+				This mod adds a selection of streets with multiple lanes, to allow overtaking, but with the same width, so you can try different road types without destroying your buildings. Some road types reserve the right lane for passenger or cargo vehicles; they carry extra signs on the pavement.
+				Use the categories to select the right street type, for ease of use.
 				[h1]Problem B: little control destroying pieces of road[/h1]
 				This mod adds two slicers to the street construction menu. They split a piece of road in two segments.
 				- The automatic slicer preserves all buildings, just plop it where you want.
 				- The manual slicer is a backup. Place it on the roadside and rotate it with <m>, <shift> + <m>, <n> or <shift> + <n>, carefully adjusting its location.
-				Both slicers destroy themselves after placement, leaving your road split in two segments. Debug mode and AltGr + L will reveal the effect.
+				Both slicers destroy themselves after placement, leaving your road split in two segments. Debug mode and AltGr + L will reveal their effect.
 				After splitting a piece of road, you can alter or delete a small segment only, preserving the rest.
 				[h1]Problem C: trams only run in the rightmost lane[/h1]
 				This mod adds a street construction to toggle extra tram tracks. Plop it on a road and it will add or remove extra tram tracks, if there are enough lanes.
@@ -19,20 +19,20 @@ function data()
 				[h1]Problem D: vehicles queue up unnecessarily[/h1]
 				The game does not offer lane-bound waypoints, so routing the vehicles might involve some trial and error.
 				[h2]Solution: use dedicated roads[/h2]
-				Replace the offending road with one of the same width, that reserves the right lane for certain vehicles. Use the standard replace tool (the magic wand), helping yourself with <shift> to select a shorter segment of road. Select the new road with the category icon to avoid confusion. Toggle bus lanes on or off as required, using the standard tool. Toggle tram tracks in the middle as required, using the dedicated street construction tool.
+				Replace the offending road with one of the same width, that reserves the right lane for certain vehicles. Use the standard replace tool (the magic wand), helping yourself with <shift> to select a shorter segment of road. Use the slicer to cut your own segments if you must. Select the new road with the category icon to avoid confusion. Toggle bus lanes on or off as required, using the standard tool. Toggle tram tracks in the middle as required, using the dedicated street construction tool.
 				[h2]Fine-grained solution: split your road into segments[/h2]
 				When you build a long enough piece of road, the game splits it into seamless segments. The joints between them allow vehicles to change lanes. The trouble is, these segments are fairly long, and crossings do not allow switching lanes. If you have a shortish stretch of road, then a crossing, then another shortish stretch, then another crossing, chances are, vehicles cannot change lanes for a long while.
 				You can split a piece of road by adding an intersecting street and deleting it, but this often involves destroying several buildings. To keep the carnage down, use the slicer. Once you have multiple segments, you can change the road in some or all of them, selectively. Try adding an extra lane to the segment with the streetside stop only.
 				[h1]Problem E: little control building roads[/h1]
 				This mod adds a "street construction" to build chunks of road with useful parameters. These chunks can be single or multiple, arranged in parallel. Lock them to keep their shape pretty and prevent other roads merging in. Unlock them to treat them like ordinary roads. You cannot relock an unlocked chunk.
-				[h1]Problem F: ugly merges[/h1]
-				This mod adds a "street construction" to build prettier merges. For example, you can merge up to four one-lane streets into a large one, perhaps for a fancy station square arrangement.
-				Merges can be reconfigured after being placed, if the street layout allows it. If a merge misbehaves after an upgrade, change its direction twice (ie reverse and restore) to update it to the latest version.
+				[h1]Problem F: ugly or impossible merges[/h1]
+				This mod adds a "street construction" to build merges. For example, you can merge up to four one-lane streets into a large one, perhaps for a fancy station square arrangement.
+				Merges can be reconfigured after being placed, if the street layout allows it. If a merge misbehaves after a mod update, change its direction twice (ie reverse and restore) to update it to the latest version.
 				[h1]Problem G: ugly tight curves[/h1]
 				This mod adds a "street construction" to build prettier tight curves. Lock a curve to keep its shape pretty and prevent other roads merging in. Unlock it to treat it like ordinary roads. You cannot relock an unlocked curve.
 				[h1]Problem H: no footpaths[/h1]
 				This mod adds some thin footpaths, nearly invisible. You can use them to perform various tricks, eg connecting stations to roads or having people walk across a park. Give them a bus lane to pedestrianise them.
-				There is also a 1 metre footpath if you want something that looks the part.
+				There is also a 1-metre footpath.
 				[h1]Problem I: vehicles make funny turns[/h1]
 				Sometimes, vehicles overshoot a crossing, then reverse, then turn where they are meant to.
 				Use the splitter to make them change lanes where you want.
@@ -41,7 +41,7 @@ function data()
 				- Press and hold <shift> to replace a short road segment between two joints.
 				- Select street - upgrade and right-click a one-way road to reverse its direction.
 				- Use the categories in the street menu to avoid headaches.
-				- The construction mover helps adjust constructions easily after plopping them.
+				- The construction mover helps adjust constructions after plopping them.
 				
 				[b]This mod may break your game if you use it and then remove it.[/b]
 				[b]Thanks to Enzojz for luadump![/b]
@@ -68,7 +68,7 @@ function data()
 				[h2]Known problems[/h2]
 				A) When you split a road near a modded street station, whose mod was removed, and then apply a modifier, such as add / remove bus lane or change the street type, the game crashes.
 				This happens with single as well as double-sided stations. You can tell those stations because the game shows a placeholder at their location.
-				This seems to be a UG problem. To solve it, replace those stations with some others available in your game.
+				This is a UG problem. To solve it, replace those stations with some others available in your game.
 				B) The game always draws the tram track on the outer lane(s), even if trams are barred there. Take this as a graphical glitch.
 				C) The game always expects buses to be allowed in the outer lane(s).
                 D) Adding a bus lane to a road can break your multi-lane paths. Trial and error is the way out of this game limitation.
