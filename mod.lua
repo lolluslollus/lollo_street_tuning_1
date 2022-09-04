abd4632628705932a47d5b00c1c0195e9f1b3fdc
lolluslollus <lolluslollus@users.noreply.github.com>
2022-09-04 15:37:43 +0200

    bump

--- mod.lua
+++ mod.lua
@@ -9,7 +9,7 @@ local streetUtils = require('lollo_street_tuning.streetUtils')
 function data()
     return {
         info = {
-            minorVersion = 58,
+            minorVersion = 59,
             severityAdd = 'NONE',
             severityRemove = 'WARNING',
             name = _('_NAME'),
