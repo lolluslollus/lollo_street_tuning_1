d682b22821ab526906986008712d0803fa222029
lolluslollus <lolluslollus@users.noreply.github.com>
2023-11-27 13:59:56 +0100

    play around with proposals

---
+++ res/scripts/lollo_street_tuning/scratch/replacingAnEdge/proposal.lua
@@ -0,0 +1,108 @@
+local proposal = {
+    addedNodes = {
+    },
+    addedSegments = {
+      [1] = {
+        entity = -1,
+        comp = {
+          node0 = 30933,
+          node1 = 30935,
+          tangent0 = {
+            x = -37.539779663086,
+            y = 51.727825164795,
+            z = -0.8017618060112,
+          },
+          tangent1 = {
+            x = -37.539779663086,
+            y = 51.727832794189,
+            z = -0.80176168680191,
+          },
+          type = 0,
+          typeIndex = -1,
+          objects = { },
+        },
+        type = 0,
+        params = {
+          streetType = 19,
+          hasBus = false,
+          tramTrackType = 0,
+          precedenceNode0 = 2,
+          precedenceNode1 = 2,
+        },
+        playerOwned = nil,
+        streetEdge = {
+          streetType = 19,
+          hasBus = false,
+          tramTrackType = 0,
+          precedenceNode0 = 2,
+          precedenceNode1 = 2,
+        },
+        trackEdge = {
+          trackType = -1,
+          catenary = false,
+        },
+      },
+    },
+    removedNodes = {
+    },
+    removedSegments = {
+      [1] = {
+        entity = 30938,
+        comp = {
+          node0 = 30933,
+          node1 = 30935,
+          tangent0 = {
+            x = -37.539779663086,
+            y = 51.727825164795,
+            z = -0.8017618060112,
+          },
+          tangent1 = {
+            x = -37.539779663086,
+            y = 51.727832794189,
+            z = -0.80176168680191,
+          },
+          type = 0,
+          typeIndex = -1,
+          objects = { },
+        },
+        type = 0,
+        params = {
+          streetType = 19,
+          hasBus = false,
+          tramTrackType = 0,
+          precedenceNode0 = 2,
+          precedenceNode1 = 2,
+        },
+        playerOwned = nil,
+        streetEdge = {
+          streetType = 19,
+          hasBus = false,
+          tramTrackType = 0,
+          precedenceNode0 = 2,
+          precedenceNode1 = 2,
+        },
+        trackEdge = {
+          trackType = -1,
+          catenary = false,
+        },
+      },
+    },
+    edgeObjectsToRemove = {
+    },
+    edgeObjectsToAdd = {
+    },
+    new2oldNodes = {
+    },
+    old2newNodes = {
+    },
+    new2oldSegments = {
+    },
+    old2newSegments = {
+    },
+    new2oldEdgeObjects = {
+    },
+    old2newEdgeObjects = {
+    },
+    frozenNodes = {
+    },
+  }
