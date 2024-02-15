07e5c5682dd4800ba541f15bc9097801f8d5b800
lolluslollus <lolluslollus@users.noreply.github.com>
2024-02-15 19:16:53 +0100

    Update transfUtils.lua

--- res/scripts/lollo_street_tuning/transfUtils.lua
+++ res/scripts/lollo_street_tuning/transfUtils.lua
@@ -374,6 +374,60 @@ local utils = {
             transf[16],
         }
     end,
+    getTransf_YSkewedOnZ = function(transf, skew)
+        local m2 = {
+            1, 0, 0, 0,
+            0, 1, skew, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1,
+        }
+
+        return {
+            transf[1],
+            transf[2],
+            transf[3],
+            transf[4],
+            transf[5] + transf[9]  * skew,
+            transf[6] + transf[10] * skew,
+            transf[7] + transf[11] * skew,
+            transf[8] + transf[12] * skew,
+            transf[9],
+            transf[10],
+            transf[11],
+            transf[12],
+            transf[13],
+            transf[14],
+            transf[15],
+            transf[16],
+        }
+    end,
+    getTransf_XSkewedOnY = function(transf, skew)
+        local m2 = {
+            1, skew, 0, 0,
+            skew, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1,
+        }
+
+        return {
+            transf[1] + transf[5] * skew,
+            transf[2] + transf[6] * skew,
+            transf[3] + transf[7] * skew,
+            transf[4] + transf[8] * skew,
+            transf[5] + transf[1] * skew,
+            transf[6] + transf[2] * skew,
+            transf[7] + transf[3] * skew,
+            transf[8] + transf[4] * skew,
+            transf[9],
+            transf[10],
+            transf[11],
+            transf[12],
+            transf[13],
+            transf[14],
+            transf[15],
+            transf[16],
+        }
+    end,
 --#endregion faster than mul()
 }
 
